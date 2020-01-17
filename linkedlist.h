42831bfe796d66543390cfc31c27253a3abb9041
umesh013 <49593186+umesh013@users.noreply.github.com>
2020-01-17 14:06:28 +0545

    Add files via upload

---
+++ linkedlist.h
@@ -0,0 +1,72 @@
+#define maxSize 7
+#include<iostream>
+class Node{
+	 public:
+	int info;
+	Node* next;
+};
+
+class List{
+	private:
+		Node* HEAD;
+		Node* TAIL;
+		public:
+		List();
+		~List();
+	
+		//functions
+		
+		bool isEmpty();
+		void addToHead(int data);
+		void addToTail(int data);
+		int removeFromHead();
+		bool isFull();
+		void traverse();
+		int itemHead();
+		int showHead();
+		int showTail();
+};
+
+
+class Queue{
+	private:
+		List l;
+	public:
+		void isEmpty(){
+			bool a = l.isEmpty();
+			if(a==true)
+			std::cout<<"Queue Empty"<<std::endl;
+			else
+			std::cout<<"Queue Not Empty!"<<std::endl;
+			
+		}
+		bool isFull(){
+			
+			if(l.isFull()== true)
+				return true;
+			else
+				return false;
+			
+		}
+		void enqueuetohead(int data){
+			l.addToHead(data);
+		}
+		void enqueue(int data){
+			l.addToTail(data);
+		}
+		void dequeue (){
+			l.removeFromHead();
+			
+		}
+		void showitems(){
+			l.traverse();
+		}
+		int rear(){
+			l.showTail();
+		}
+		int front(){
+			l.showHead();
+		}
+};
+
+
---
+++ list.cpp
@@ -0,0 +1,149 @@
+
+#include<iostream>
+#include "linkedlist.h"
+using namespace std;
+
+
+List::List(){
+	HEAD = NULL;
+	TAIL = NULL;
+	
+}
+
+List::~List(){}
+
+bool List::isEmpty(){
+	if (HEAD==NULL && TAIL == NULL){
+		return true;
+	}
+	else
+	{
+		return false;
+		}
+}
+bool List::isFull(){
+	int p=0;
+		Node* temp = HEAD;
+	
+	while(temp!=NULL){
+		p++;
+		temp = temp-> next;
+		if(temp==HEAD){
+		    break;
+		}
+		
+	}
+	if(p<maxSize)
+		return false;
+	else
+		return true;
+}
+
+void List::addToHead(int data){
+	Node* newNode= new Node();
+	newNode ->info = data;
+	newNode ->next = NULL;
+	HEAD = newNode;
+	if(TAIL==NULL){
+		TAIL= HEAD;
+	}
+	
+}  
+
+void List::addToTail(int data){
+	Node* newnode = new Node();
+	newnode->info = data;
+	newnode->next = NULL;
+	TAIL->next = newnode;
+	TAIL = newnode;
+	
+    if(HEAD==NULL){
+        HEAD=TAIL;
+    }
+    else{
+        TAIL->next = HEAD;
+    }
+	
+}
+
+
+int List::removeFromHead(){
+	Node* nodeToDelete;
+	nodeToDelete =HEAD;
+	int num = HEAD->info;
+	HEAD = nodeToDelete->next;
+	TAIL->next = HEAD;
+	return num;
+}
+
+
+void List::traverse(){
+	Node* temp = HEAD;
+	
+	while(temp!=NULL){
+		cout<< temp->info<<"\t";
+		temp = temp-> next;
+		if(temp==HEAD){
+		    return;
+		}
+		
+	}
+	
+}
+
+int List::showHead(){
+	return HEAD-> info;
+}
+int List::showTail(){
+	return TAIL -> info;
+}
+
+
+
+
+
+
+
+
+int main(){
+
+	
+	
+
+	
+//IMPLEMENTING QUEUE
+
+	cout<<"\nQueue Implementation:"<<endl;
+	Queue qu;
+	qu.isEmpty();
+	qu.enqueuetohead(4);
+	qu.dequeue();
+	for(int i=1;i<15;i=i+2){
+		if(qu.isFull() == false)
+			qu.enqueue(i);
+		else{
+			cout<<"Queue is full !";
+			break;
+		}
+			
+	}
+	//qu.isFull();
+	if(qu.isFull() == true)
+		cout<<" queue full  ";
+	else
+		cout<<"Not full ";
+	
+	//qu.dequeue();
+	
+	qu.isEmpty();
+	qu.showitems();
+	cout<<"\n Rear and front:";
+	cout<<qu.rear()<<"  ";
+	cout<< qu.front();
+	
+	return 0;
+	
+	
+}
+
+
